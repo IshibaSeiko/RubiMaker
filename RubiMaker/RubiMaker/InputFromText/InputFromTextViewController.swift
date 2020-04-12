7a8e7b92879959674a81ed26ac571bd65c552242
seiko <seikoseikou.com@gmail.com>
2020-04-12 21:36:48 +0900

    入力画面を作成

---
+++ RubiMaker/RubiMaker/InputFromText/InputFromTextViewController.swift
@@ -0,0 +1,24 @@
+//
+//  InputFromTextViewController.swift
+//  RubiMaker
+//
+//  Created by 石場清子 on 2020/04/12.
+//  Copyright © 2020 石場清子. All rights reserved.
+//
+
+import UIKit
+
+class InputFromTextViewController: UIViewController {
+
+    // MARK: - IBOutlet
+    @IBOutlet private weak var scrollView: UIScrollView!
+    @IBOutlet private weak var inputTextView: UITextView!
+    @IBOutlet private weak var convertedTextView: UITextView!
+    @IBOutlet private weak var convertButton: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
