33a6c82d941ebdb95632d20e47da8df80eb5f15a
seiko <seikoseikou.com@gmail.com>
2020-04-12 17:55:34 +0900

    履歴表示画面を作成してアプリ起動時に表示されるよう変更、フォルダを分割

---
+++ RubiMaker/RubiMaker/History/HistoryViewController.swift
@@ -0,0 +1,19 @@
+//
+//  HistoryViewController.swift
+//  RubiMaker
+//
+//  Created by 石場清子 on 2020/04/12.
+//  Copyright © 2020 石場清子. All rights reserved.
+//
+
+import UIKit
+
+class HistoryViewController: UIViewController {
+
+    @IBOutlet weak var historyTableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
