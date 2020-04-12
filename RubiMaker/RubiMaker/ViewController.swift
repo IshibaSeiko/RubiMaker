db8f696f17c5d6afd7868e17f22f0a91d0fecfce
seiko <seikoseikou.com@gmail.com>
2020-04-12 16:58:39 +0900

    プロジェクトを作成

---
+++ RubiMaker/RubiMaker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RubiMaker
+//
+//  Created by 石場清子 on 2020/04/12.
+//  Copyright © 2020 石場清子. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
