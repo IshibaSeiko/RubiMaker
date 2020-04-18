8be7fb9bfdfb7fee65f047b81f67ac9fbd4d469a
seiko <seikoseikou.com@gmail.com>
2020-04-18 15:37:56 +0900

    TextView、ButtonのUIを調整

--- RubiMaker/RubiMaker/InputFromText/InputFromTextViewController.swift
+++ RubiMaker/RubiMaker/InputFromText/InputFromTextViewController.swift
@@ -17,6 +17,7 @@ final class InputFromTextViewController: UIViewController {
     // MARK: - IBOutlet
     @IBOutlet weak var inputTextView: UITextView!
     @IBOutlet private weak var convertedTextView: UITextView!
+    @IBOutlet weak var convertButtonBackView: UIView!
     @IBOutlet private weak var convertButton: UIButton!
     @IBOutlet private weak var bottomView: UIView!
 
@@ -30,6 +31,19 @@ final class InputFromTextViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         inputTextView.delegate = self
+
+        inputTextView.layer.borderWidth = 0.5
+        inputTextView.layer.borderColor = UIColor.gray.cgColor
+        inputTextView.layer.cornerRadius = 5.0
+
+        convertedTextView.layer.borderWidth = 0.5
+        convertedTextView.layer.borderColor = UIColor.gray.cgColor
+        convertedTextView.layer.cornerRadius = 5.0
+
+        convertButton.layer.borderWidth = 0.5
+        convertButton.layer.borderColor = UIColor.gray.cgColor
+        convertButton.layer.cornerRadius = 15.0
+
         convertedTextView.isEditable = false
         convertAPI.returnCodeResult = self
         convertButton.setTitle("Convert", for: .normal)
@@ -56,7 +70,7 @@ final class InputFromTextViewController: UIViewController {
 // MARK: - Instance Method
 extension InputFromTextViewController {
     func isFull(_ isFull: Bool) {
-        convertButton.isHidden = !isFull
+        convertButtonBackView.isHidden = !isFull
         convertedTextView.isHidden = !isFull
         bottomView.isHidden = isFull
     }
