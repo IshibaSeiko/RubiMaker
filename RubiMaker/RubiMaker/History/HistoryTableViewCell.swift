a99cc28682538af856384089bde8a165b7e3fb28
seiko <seikoseikou.com@gmail.com>
2020-04-18 22:09:49 +0900

    履歴テーブルセルのUIを変更

--- RubiMaker/RubiMaker/History/HistoryTableViewCell.swift
+++ RubiMaker/RubiMaker/History/HistoryTableViewCell.swift
@@ -10,15 +10,24 @@ import UIKit
 
 class HistoryTableViewCell: UITableViewCell {
 
+    @IBOutlet private weak var convertedTextLabel: UILabel!
+    @IBOutlet private weak var inputTextLabel: UILabel!
+    @IBOutlet weak var favoriteButton: UIButton!
+
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
     }
-    
+
+    func setCell(_ convertEntity: ConvertEntity) -> HistoryTableViewCell {
+        favoriteButton.setImage(UIImage(systemName: "star")?.withRenderingMode(.alwaysTemplate), for: .normal)
+        favoriteButton.setImage(UIImage(systemName: "star.fill")?.withRenderingMode(.alwaysTemplate), for: .selected)
+        convertedTextLabel.text = convertEntity.converted
+        inputTextLabel.text = convertEntity.input
+        favoriteButton.isSelected = convertEntity.favoriteFlg
+        return self
+    }
 }
